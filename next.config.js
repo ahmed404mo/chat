b9780a4680dce388f45ad58715440389a842ad75
ahmed mokhtar <mo879938@gmail.com>
2026-06-27 16:37:33 +0300

    done edit 25

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   sassOptions: {
     includePaths: [path.join(__dirname, "node_modules")],
     silenceDeprecations: ["import", "legacy-js-api"],
